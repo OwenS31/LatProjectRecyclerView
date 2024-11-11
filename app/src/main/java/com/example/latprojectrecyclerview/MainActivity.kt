5ddf919ec85d9143bfb6dc67240d0c9521a1cb9b
Owen Samuel <owensamuel31@gmail.com>
2024-11-12 01:24:43 +0700

    finalize

--- app/src/main/java/com/example/latprojectrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/latprojectrecyclerview/MainActivity.kt
@@ -5,8 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _nama : Array<String>
+    private lateinit var _deskripsi : Array<String>
+    private lateinit var _karakter : Array<String>
+    private lateinit var _gambar : Array<String>
+
+    private var arWayang = arrayListOf<wayang>()
+    private lateinit var _rvWayang : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +29,35 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        _rvWayang = findViewById(R.id.rvWayang)
+        SiapkanData()
+        TambahData()
+        TampilkanData()
+    }
+
+    fun SiapkanData(){
+        _nama = resources.getStringArray(R.array.namaWayang)
+        _deskripsi = resources.getStringArray(R.array.deskripsiWayang)
+        _karakter = resources.getStringArray(R.array.karakterUtamaWayang)
+        _gambar = resources.getStringArray(R.array.gambarWayang)
+    }
+
+    fun TambahData(){
+        for (position in _nama.indices){
+            val data = wayang(
+                _gambar[position],
+                _nama[position],
+                _karakter[position],
+                _deskripsi[position]
+            )
+            arWayang.add(data)
+        }
+    }
+
+    fun TampilkanData(){
+        _rvWayang.layoutManager = StaggeredGridLayoutManager(2,LinearLayoutManager.VERTICAL)
+//        _rvWayang.layoutManager = GridLayoutManager(this,2)
+//        _rvWayang.layoutManager = LinearLayoutManager(this)
+        _rvWayang.adapter = adapterRecView(arWayang)
     }
 }
